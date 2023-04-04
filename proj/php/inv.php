0509d8639d3459b8adeae2a5cedc529f90f112bb
Davadf <gd64553@gmail.com>
2023-04-04 09:52:33 +0200

    adition of php

---
+++ proj/php/inv.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+<?php
+$username = 'John'; // Example username
+include 'header.php';
+?>
+<h1>Welcome to my website, <?php echo $username; ?>!</h1>
+<p>This is the content of my website.</p>
+</body>
+</html>
+
+
---
+++ proj/php/test.php
@@ -0,0 +1,19 @@
+
+<?php
+    $username = "John";
+
+    ?>
+<?php
+// Start the session
+session_start();
+?>
+
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
+
+ <?php echo $username; ?>
